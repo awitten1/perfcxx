9f4dd9a9d7fbe8834e3083e359459e8493af5627
Andrew Witten <awitten007@gmail.com>
2025-05-26 17:18:00 -0400

    cool!

---
+++ example.cpp
@@ -0,0 +1,83 @@
+
+#include "perf-lib.hpp"
+#include <algorithm>
+#include <cstdint>
+#include <cstdlib>
+#include <linux/perf_event.h>
+#include <numeric>
+#include <unordered_map>
+#include <vector>
+
+int main() {
+
+    int vector_size = 100000;
+
+
+    std::cout << "sequential access pattern" << std::endl;
+    {
+        std::vector<uint64_t> vec(vector_size);
+        std::iota(vec.begin(), vec.end(), 0);
+        PerfEventGroup perf_event_group(PERF_COUNT_HW_CPU_CYCLES, PERF_TYPE_HARDWARE, "cycles");
+        perf_event_group.AddEvent(PERF_COUNT_HW_INSTRUCTIONS, PERF_TYPE_HARDWARE, "ins");
+        perf_event_group.AddEvent(PERF_COUNT_HW_CACHE_L1D |
+                (PERF_COUNT_HW_CACHE_OP_READ << 8) |
+                (PERF_COUNT_HW_CACHE_RESULT_MISS << 16), PERF_TYPE_HW_CACHE, "l1d-cache-misses");
+
+        perf_event_group.AddEvent(PERF_COUNT_HW_CACHE_L1D |
+                (PERF_COUNT_HW_CACHE_OP_READ << 8) |
+                (PERF_COUNT_HW_CACHE_RESULT_ACCESS << 16), PERF_TYPE_HW_CACHE, "l1d-cache-accesses");
+
+        perf_event_group.Enable();
+
+        uint64_t res = 0;
+        for (int i = 0; i < vec.size(); ++i) {
+            res += vec[i];
+        }
+
+        auto results = perf_event_group.ReadEvents();
+        std::cout << res << std::endl;
+
+        for (const auto [key, value] : results) {
+            std::cout << key << ", " << value << std::endl;
+        }
+    }
+
+
+    std::cout << "random access pattern" << std::endl;
+    {
+        std::vector<uint64_t> vec(vector_size);
+        std::iota(vec.begin(), vec.end(), 0);
+        uint32_t seed = 12345;
+
+        PerfEventGroup perf_event_group(PERF_COUNT_HW_CPU_CYCLES, PERF_TYPE_HARDWARE, "cycles");
+        perf_event_group.AddEvent(PERF_COUNT_HW_INSTRUCTIONS, PERF_TYPE_HARDWARE, "ins");
+        perf_event_group.AddEvent(PERF_COUNT_HW_CACHE_L1D |
+                (PERF_COUNT_HW_CACHE_OP_READ << 8) |
+                (PERF_COUNT_HW_CACHE_RESULT_MISS << 16), PERF_TYPE_HW_CACHE, "l1d-cache-misses");
+
+        perf_event_group.AddEvent(PERF_COUNT_HW_CACHE_L1D |
+                (PERF_COUNT_HW_CACHE_OP_READ << 8) |
+                (PERF_COUNT_HW_CACHE_RESULT_ACCESS << 16), PERF_TYPE_HW_CACHE, "l1d-cache-accesses");
+
+
+        perf_event_group.Enable();
+
+        uint64_t res = 0;
+        for (int i = 0; i < vec.size(); ++i) {
+            seed = seed * 1103515245 + 12345;
+            res += vec[seed % vec.size()];
+        }
+
+        auto results = perf_event_group.ReadEvents();
+        std::cout << res << std::endl;
+
+        for (const auto [key, value] : results) {
+            std::cout << key << ", " << value << std::endl;
+        }
+    }
+
+
+
+
+    return 0;
+}
--- perf-lib.hpp
+++ perf-lib.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <algorithm>
+#include <cstring>
+#include <iostream>
 #include <linux/perf_event.h>
 #include <linux/hw_breakpoint.h>
 #include <sstream>
@@ -13,10 +16,6 @@
 #include <string>
 #include <thread>
 
-#include "nlohmann/json.hpp"
-
-using namespace std::chrono_literals;
-
 // https://man7.org/linux/man-pages/man2/perf_event_open.2.html
 class PerfEventGroup {
     int fd_;
@@ -25,13 +24,17 @@ class PerfEventGroup {
         uint64_t config;
         uint64_t id;
         std::string event_name;
+        int fd;
     };
     std::vector<EventInfo> event_info_map_;
 
     uint64_t GetEventId(int fd) {
         uint64_t event_id;
         int ret = ioctl(fd, PERF_EVENT_IOC_ID, &event_id);
-        assertAndFormat(ret, "failed to get event id");
+        if (ret == -1) {
+            std::cerr << "failed to get event id" << std::endl;
+            return -1;
+        }
         return event_id;
     }
 
@@ -48,40 +51,37 @@ class PerfEventGroup {
 
         // disable leader initially.  when leader starts, everyone starts.
         pe.disabled = leader ? 1 : 0;
-
         pe.sample_period = 0;
-
         pe.read_format = PERF_FORMAT_GROUP | PERF_FORMAT_ID;
         pe.exclude_hv = 1;
-
-        pe.exclude_kernel = 1;
-
+        pe.exclude_kernel = 0;
         unsigned long flags = 0;
-
         int cpu = -1; // any cpu
         pid_t pid = 0; // this process
-
         int group_fd = leader ? -1 : fd_;
         int fd = syscall(SYS_perf_event_open, &pe, pid, cpu, group_fd, flags);
         if (leader) {
             fd_ = fd;
         }
-        assertAndFormat(fd, "failed opening perf event");
-        event_info_map_.push_back(EventInfo{.config = config, .id = GetEventId(fd), .event_name = event_name});
+        if (fd == -1) {
+            std::cerr << "failed opening perf event, reason = " << strerror(errno) << std::endl;
+            return;
+        }
+        event_info_map_.push_back(EventInfo{
+            .config = config, .id = GetEventId(fd), .event_name = event_name, .fd = fd});
     }
 
-    void assertAndFormat(int ret, const char* msg) {
-        if (ret == -1) {
-            std::ostringstream os;
-            os << msg << " :" << strerror(errno) << std::endl;
-            throw std::runtime_error{os.str()};
-        }
-    }
 
 public:
 
     explicit PerfEventGroup(uint64_t config, uint32_t type, const std::string& name) {
         AddEvent(true, config, type, name);
+    }
+
+    ~PerfEventGroup() {
+        for (auto it = event_info_map_.rbegin(); it != event_info_map_.rend(); ++it) {
+            close(it->fd);
+        }
     }
 
     int GetFd() {
@@ -110,17 +110,23 @@ public:
         // read returns an array of {value, event_id tuples}
         std::vector<uint64_t> events(1 + 2 * NumEvents());
         int ret = read(fd_, events.data(), events.size() * 8);
-        assertAndFormat(ret, "failed to read events");
-        if (events[0] != NumEvents()) {
-            throw std::runtime_error{"number of events recieved does not equal expected number"};
+        if (ret == -1) {
+            std::ostringstream oss;
+            oss << "failed to read events reason = " << strerror(errno);
+            std::cerr << oss.str() << std::endl;
+            return {};
         }
+        if (events[0] != NumEvents()) {
+            std::cerr << "number of events recieved does not equal expected number" << std::endl;
+            return {};
+        }
+
         uint64_t id = -1;
         uint64_t value = -1;
         // skip first element, which holds nr, the number of events
         for (size_t i = 1; i < events.size(); ++i) {
             if ((i & 1) == 0) {
                 id = events[i];
-                std::cout << id << std::endl;
                 auto it = std::find_if(event_info_map_.begin(), event_info_map_.end(),
                     [id](auto v) { return v.id == id; });
                 if (it == event_info_map_.end()) {
@@ -136,11 +142,11 @@ public:
 
     void Enable() {
         // is PERF_IOC_FLAG_GROUP needed when operating on group leader?
-        int ret = ioctl(fd_, PERF_IOC_FLAG_GROUP | PERF_EVENT_IOC_RESET);
+        int ret = ioctl(fd_,  PERF_EVENT_IOC_RESET);
         if (ret == -1) {
             throw std::runtime_error{"failed to reset counters"};
         }
-        ret = ioctl(fd_, PERF_IOC_FLAG_GROUP | PERF_EVENT_IOC_ENABLE);
+        ret = ioctl(fd_,  PERF_EVENT_IOC_ENABLE);
         if (ret == -1) {
             throw std::runtime_error{"failed to enable events"};
         }
@@ -153,33 +159,9 @@ public:
     void Reset() {
         int ret = ioctl(fd_, PERF_IOC_FLAG_GROUP | PERF_EVENT_IOC_RESET);
         if (ret == -1) {
-            throw std::runtime_error{"failed to reset counters"};
+            std::ostringstream oss;
+            oss << "failed to reset counters";
+            std::cerr << oss.str() << std::endl;
         }
     }
-};
-
-class PerfDumper {
-    PerfEventGroup perf_event_group_;
-    std::thread t_;
-    bool enabled = false;
-
-    void LogEvents() {
-        auto events = perf_event_group_.ReadEvents();
-        nlohmann::json j = events;
-        std::cout << j.dump() << std::endl;
-    }
-
-public:
-    PerfDumper(std::vector<PerfEventGroup::EventArg> args) :
-        perf_event_group_(PerfEventGroup::BuildPerfEventGroup(std::move(args))),
-        t_([this]() {
-            for (;;) {
-                if (!enabled) {
-                    perf_event_group_.Enable();
-                }
-                LogEvents();
-                enabled = true;
-                std::this_thread::sleep_for(500ms);
-            }
-        }) { t_.detach(); std::cout << "started thread for perf dumper" << std::endl; }
 };
